83c97336d7dc6cf068e037f31b5b41366c11c747
Jay Morrow <jay.m.morrow@gmail.com>
2013-02-18 18:34:23 -0600

    Adding Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,103 @@
+
+module.exports = function(grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        pkg: grunt.file.readJSON('validatr.jquery.json'),
+
+        banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+        
+        // Task configuration.
+        clean: {
+            files: ['dist']
+        },
+
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            dist: {
+                src: ['src/js/<%= pkg.name %>.js'],
+                dest: 'dist/src/<%= pkg.name %>.js'
+            },
+            distcss: {
+                src: ['src/css/<%= pkg.name %>.css'],
+                dest: 'dist/src/<%= pkg.name %>.css'
+            }
+        },
+
+        cssmin: {
+            dist: {
+                files: {
+                    'dist/min/<%= pkg.name %>.min.css': ['<%= concat.distcss.dest %>']
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= concat.dist.dest %>',
+                dest: 'dist/min/<%= pkg.name %>.min.js'
+            }
+        },
+
+        jshint: {
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                devel: true,
+                globals: {
+                    jQuery: true,
+                    module: true
+                }
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            src: {
+                src: ['src/**/*.js']
+            }
+        },
+
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            src: {
+                files: '<%= jshint.src.src %>',
+                tasks: ['jshint:src']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'clean', 'concat', 'cssmin', 'uglify']);
+};
